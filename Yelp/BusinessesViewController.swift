c0598ba4d6ca51167278d12bf5d411d02959d39e
Eli Tucker <eli@tumblr.com>
2015-09-08 19:48:59 -0700

    Added filtering for categories (for real this time)

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, FiltersViewControllerDelegate {
 
     var businesses: [Business]!
+    var searchTerm = "Restaurants"
+    var searchCategories: [String]? = nil
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -28,49 +30,43 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 140 // only used for scrollbar row height dimension
         
-//        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
-//            self.businesses = businesses
-//            
-//            for business in businesses {
-//                println(business.name!)
-//                println(business.address!)
-//            }
-//        })
-        
-
-        search("Restaurants")
-//        Business.searchWithTerm("Restaurants", sort: .Distance,
-//            categories: nil, //["thai", "asianfusion", "burgers"],
-//            deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
-//            self.businesses = businesses
-//            self.tableView.reloadData()
-//            
-////            for business in businesses {
-////                println(business.name!)
-////                println(business.address!)
-////            }
-//        }
+        search()
     }
     
-    func search(term: String) {
-        Business.searchWithTerm(term, sort: .Distance,
-            categories: nil, //["thai", "asianfusion", "burgers"],
-            deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
-                self.businesses = businesses
-                self.tableView.reloadData()
+    func search() {
+        doSearch(searchTerm, categories: searchCategories)
+    }
 
-                for business in businesses {
-                    println(business.name!)
-                    println(business.address!)
-                }
+    func doSearch(term: String, categories: [String]? = nil) {
+        Business.searchWithTerm(term, sort: .Distance,
+            categories: categories, //["thai", "asianfusion", "burgers"],
+            deals: true) { (businessesReturned: [Business]!, error: NSError!) -> Void in
+                println(error)
+                self.updateBusinessList(businessesReturned)
         }
+    }
+    
+    func updateBusinessList(businesses: [Business]) {
+        self.businesses = businesses
+        self.tableView.reloadData()
+
+//        for business in businesses {
+//            println(business.name!)
+//            println(business.address!)
+//        }
     }
     
     // --- UISearchBarDelegate handling
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        println("---")
-        println("Search Clicked!")
-        search(searchBar.text)
+        searchTerm = searchBar.text
+        search()
+    }
+    
+    // TODO: Why doesn't this get called?
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        println("Search was cancelled")
+        searchTerm = "Restaurants"
+        search()
     }
     // --- end UISearchBarDelegate handling
     
@@ -97,10 +93,18 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
     */
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let navigationController = segue.destinationViewController as! UINavigationController
+        let filtersViewController = navigationController.topViewController as! FiltersViewController
+        filtersViewController.delegate = self
+    }
+    
+    func filtersViewController(filtersViewController: FiltersViewController, didUpdateFilters filters: [String : AnyObject]) {
+        let categories = filters["categories"] as? [String]
+        
+        searchCategories = categories
+        search()
+    }
 
 }
--- Yelp/SwitchCell.swift
+++ Yelp/SwitchCell.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class SwitchCell: UITableViewCell {
+@objc protocol SwitchCellDelegate {
+    optional func switchCell(switchCell: SwitchCell, didChangeValue value: Bool)
+}
 
+class SwitchCell: UITableViewCell {
+    
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var onSwitch: UISwitch!
+    
+    weak var delegate: SwitchCellDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        onSwitch.addTarget(self, action: "switchValueChanged", forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func switchValueChanged() {
+        println("something changed!")
+        delegate?.switchCell?(self, didChangeValue: onSwitch.on)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
